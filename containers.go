990c725523cc943a9d350ee7f40eb94bf4d62e55
amidgo <mcdvaran@gmail.com>
2024-12-26 14:11:28 +0400

    add Disabled func

--- containers.go
+++ containers.go
@@ -6,12 +6,16 @@ import (
 	"testing"
 )
 
-func SkipDisabled(t *testing.T) {
+func Disabled() bool {
 	env := os.Getenv("CONTAINERS_DISABLE_TESTING")
 
 	disabled, _ := strconv.ParseBool(env)
 
-	if disabled {
-		t.Skipf("test skipped because CONTAINERS_DISABLE_TESTING=%s", env)
+	return disabled
+}
+
+func SkipDisabled(t *testing.T) {
+	if Disabled() {
+		t.Skipf("test skipped because CONTAINERS_DISABLE_TESTING is SET to TRUE")
 	}
 }
