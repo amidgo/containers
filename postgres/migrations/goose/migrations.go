8769c6f04042120442c617f3e7be4e546ce2acec
amidgo <mcdvaran@gmail.com>
2025-03-21 12:26:25 +0400

    add support for embed migrations

--- postgres/migrations/goose/migrations.go
+++ postgres/migrations/goose/migrations.go
@@ -3,21 +3,48 @@ package goosemigrations
 import (
 	"context"
 	"database/sql"
+	"fmt"
+	"io/fs"
+	"os"
 
 	"github.com/amidgo/containers/postgres/migrations"
 	"github.com/pressly/goose/v3"
 )
 
 type gooseMigrations struct {
-	folder string
+	fs fs.FS
 }
 
 func New(folder string) migrations.Migrations {
 	return gooseMigrations{
-		folder: folder,
+		fs: os.DirFS(folder),
+	}
+}
+
+func Embed(fs fs.FS) migrations.Migrations {
+	return gooseMigrations{
+		fs: fs,
 	}
 }
 
 func (g gooseMigrations) Up(ctx context.Context, db *sql.DB) error {
-	return goose.UpContext(ctx, db, g.folder)
+	gooseProvider, err := goose.NewProvider(goose.DialectPostgres, db, g.fs)
+	if err != nil {
+		return fmt.Errorf("create provider, %w", err)
+	}
+
+	report, err := gooseProvider.Up(ctx)
+	if err != nil {
+		return fmt.Errorf("up provider migrations, %w", err)
+	}
+
+	for _, r := range report {
+		if r.Error == nil {
+			continue
+		}
+
+		return err
+	}
+
+	return nil
 }
